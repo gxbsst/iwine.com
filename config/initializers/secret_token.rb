8386d4850be3f6ecc7dbfeebe5acac1169f11385
Weston <weixuhong@gmail.com>
2012-02-20 13:20:36 +0800

    INITIALIZE PROJECT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patrick::Application.config.secret_token = 'ad1f4dd10b382c3e0d954afbfd27d76abf4f562c267c4d46faa2328447a620ea331dc3d35364f9952e6d5dc4b1ecf7824dd62e315797961851cbe869af23c28e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Patrick::Application.config.session_store :cookie_store, key: '_Patrick_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Patrick::Application.config.session_store :active_record_store
