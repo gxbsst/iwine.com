322f321a5c78e628f8240dadf6a78dcb813dc947
Roy Dong <roy.dongwei@gmail.com>
2012-03-08 18:08:31 +0800

    wine

--- app/models/region_tree.rb
+++
@@ -1,6 +0,0 @@
-class RegionTree < ActiveRecord::Base
-  self.table_name = 'wine_region_trees'
-  
-  #has_many :children, :class_name => "RegionTree", :foreign_key => "parent_id" #, :select => [:name_en, :name_zh]
-  #belongs_to :parent, :class_name => "RegionTree", :foreign_key => "parent_id"
-end
