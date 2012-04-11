d49e2abad18ecb8a93eda0b46f08ea4d80ea8943
Weston <weixuhong@gmail.com>
2012-04-11 11:54:06 +0800

    merge branch develop
    Signed-off-by: Weston <weixuhong@gmail.com>

--- app/models/users/profile.rb
+++ app/models/users/profile.rb
@@ -1,23 +1,23 @@
+# -*- coding: utf-8 -*-
 class Users::Profile < ActiveRecord::Base
   include Users::UserSupport
   belongs_to :user
-<<<<<<< HEAD
-  
+
   attr_accessor :username
-  
+
   # validates_numericality_of :qq,  :message => "数据格式不对", :allow_nil => true
   validates :contact_email, :msn, :email_format => true, :allow_blank => true
-  
+
   validates :qq, :presence => false, :allow_blank => true, :numericality => true
-  
+
   validates :username, :presence => true
-  
+
   delegate :username, :to => :user
- # validates_numericality_of :qq, :message => "请输入正确的格式"  
+ # validates_numericality_of :qq, :message => "请输入正确的格式"
   # attr_protected :_config
   #attr_accessible :province, :city, :district
   store_configurable
-  
+
   def get_living_city_path
     region = Region.find(living_city)
     parent = region.parent
@@ -29,8 +29,8 @@ class Users::Profile < ActiveRecord::Base
     end
     path.reverse!
   end
-  
+
 end
-=======
-end
->>>>>>> develop
+
+
+
