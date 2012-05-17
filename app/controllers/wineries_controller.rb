7a2db9461e6211e8ebe0498eb292d4fda668ddbb
sidways <sidways@Sidways-MAC-Mini-002.local>
2012-05-17 16:25:24 -0700

    winery show

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -2,4 +2,8 @@ class WineriesController < ApplicationController
   def index
       @timelines = Wines::Detail.timeline_events.page(params[:page] || 1 ).per(6)
   end
+
+  def show
+    @winery = Winery.includes([:info_items, :photos]).find(params[:id])
+  end
 end
