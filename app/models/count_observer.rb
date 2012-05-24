130c539c5dbc2b3a9d08e07d78e48b94a08f8650
sidways <sidways@Sidways-MAC-Mini-002.local>
2012-05-23 17:48:41 -0700

    修改 model 多继承导致的 count_oberver bug

--- app/models/count_observer.rb
+++ app/models/count_observer.rb
@@ -113,11 +113,17 @@ class CountObserver < ActiveRecord::Observer
   end
 
   def change_to_pluralize model
-    model.class.name.downcase.pluralize
+    name = get_model_name model
+    name.downcase.pluralize
+  end
+
+  def get_model_name model
+    model.class.name.include?("Comment") ? model.class.superclass.name : model.class.name
   end
 
   def get_target model
-    target_id, target_type, target_type_class = case model.class.name
+    name = get_model_name model
+    target_id, target_type, target_type_class = case name
       when "Photo"
         [model.imageable_id, "Photo", model.imageable_type]
       when "Comment"
