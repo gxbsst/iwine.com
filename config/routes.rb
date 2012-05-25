60fcd81462611c8cb9a42831e537c93ccd624ec8
sidways <sidways@Sidways-MAC-Mini-002.local>
2012-05-24 17:18:48 -0700

    count 计数功能增加并改进， 酒庄的相册，酒，评论列表

--- config/routes.rb
+++ config/routes.rb
@@ -143,6 +143,7 @@ Patrick::Application.routes.draw do
   resources :wineries do
     member do
       get "wines_list"
+      get "followers_list"
     end
     resources :photos
     resources :comments, :controller => "comments" do
