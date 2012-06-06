ee7e7bfb1d2f47bd4926621ee82bbaf19aa13764
sidways <sidways@Sidways-MAC-Mini-002.local>
2012-06-06 14:40:04 -0700

    fixed: bug #288

--- app/controllers/cellars_controller.rb
+++ app/controllers/cellars_controller.rb
@@ -3,23 +3,32 @@ class CellarsController < ApplicationController
 
   before_filter :get_user
   before_filter :get_cellar
+  before_filter :get_order_type
 
   def show
     if params[:wine_name]
       if mine_equal_current_user?(@user)
         @cellar_items = @cellar.mine_items.joins(:wine_detail => :wine).
             where("wines.name_en like ? or wines.name_zh like ? ", "%#{params[:wine_name]}%", "%#{params[:wine_name]}%").
+            order(@order).
             page params[:page] || 1
       else
         @cellar_items = @cellar.user_items.joins(:wine_detail => :wine).
             where("wines.name_en like ? or wines.name_zh like ? ", "%#{params[:wine_name]}%", "%#{params[:wine_name]}%").
+            order(@order).
             page params[:page] || 1
       end
     else
       if mine_equal_current_user?(@user)
-        @cellar_items = @cellar.mine_items.page params[:page] || 1
+        @cellar_items = @cellar.mine_items.
+            joins(:wine_detail).
+            order(@order).
+            page params[:page] || 1
       else
-        @cellar_items = @cellar.user_items.page params[:page] || 1
+        @cellar_items = @cellar.user_items.
+            joins(:wine_detail).
+            order(@order).
+            page params[:page] || 1
       end
     end
   end
@@ -34,5 +43,12 @@ class CellarsController < ApplicationController
     @user = User.find(params[:id])
   end
 
-
+  def get_order_type
+    order_hash = { "1" => "buy_date asc", "2" => "buy_date desc", "3" => "year asc", "4" => "year desc", "5" => "price asc", "6" => "price desc"}
+    if params[:search_order]
+      @order = order_hash[params[:search_order]]
+    else
+      @order = "buy_date asc"
+    end
+  end
 end
