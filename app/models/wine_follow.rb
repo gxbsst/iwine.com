980eed2a3dd6e955a2f58ae3db9e6062b61d6b17
sidways <sidways@Sidways-MAC-Mini-002.local>
2012-06-21 20:25:35 -0700

    debug

--- app/models/wine_follow.rb
+++ app/models/wine_follow.rb
@@ -1,6 +1,6 @@
 class WineFollow < ::Follow
 	fires :new_follow,  :on              => :create,
 	                    :actor           => :user,
-	                    :secondary_actor => :followable,
+	                    :secondary_actor => :followable
 
 end
