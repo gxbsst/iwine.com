0fb3193180368aa1ca6bb2fd7cf8339614d46e23
sidways <sidways@Sidways-MAC-Mini-002.local>
2012-06-28 09:39:40 -0700

    解决用户无法注册

--- app/models/user.rb
+++ app/models/user.rb
@@ -165,7 +165,7 @@ class User < ActiveRecord::Base
   validates :agree_term, :acceptance => true, :on => :create
   validates :email, :username, :uniqueness => true
   validates :username, :presence => true
-  validates :city, :presence => true
+  # validates :city, :presence => true
   # validates :current_password, :presence => true
   validates_confirmation_of :password
 
