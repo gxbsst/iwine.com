5fdd1330036c9b1b77bc153246d6b7bf07951874
shanhua <shanhua.chen@sidways.com>
2012-07-13 13:34:16 +0800

    #356

--- db/migrate/20120712074007_add_price_type_to_user_wine_cellar_items.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceTypeToUserWineCellarItems < ActiveRecord::Migration
-  def change
-    add_column :user_wine_cellar_items, :price_type, :string, :default => "CNY", :limit => 30
-  end
-end
