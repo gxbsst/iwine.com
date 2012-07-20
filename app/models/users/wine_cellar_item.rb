ea6d30bdc73d90f56ab155926c476fb009b5edb0
xiaofei li <xiaofei.li@sidways.com>
2012-07-20 15:38:37 +0800

    我的酒窖   导出excel

--- app/models/users/wine_cellar_item.rb
+++ app/models/users/wine_cellar_item.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+# require 'icov'
 class Users::WineCellarItem < ActiveRecord::Base
     include Users::UserSupport
     belongs_to :wine_cellar, :class_name => 'Users::WineCellar', :foreign_key => 'user_wine_cellar_id'
@@ -22,6 +23,43 @@ class Users::WineCellarItem < ActiveRecord::Base
                         :actor           => :user,
                         :secondary_actor => :wine_detail,
                         :if => lambda {|item| item.user.profile.config.share.wine_cellar.to_i == 1}
-    
-  
+
+
+
+ def self.to_csv(options = {})
+  # CSV.generate(options) do |csv|
+  #   head = 'EF BB BF'.split(' ').map{|a|a.hex.chr}
+  #   csv << head
+  #   csv << column_names
+  #   all.each do |product|
+  #     csv << product.attributes.values_at(*column_names)
+  #   end
+  # end
+
+  # write_content = Iconv.conv("utf-16le", "utf-8", "\xEF\xBB\xBF")
+  # write_content += Iconv.conv("utf-16le", "utf-8", csv_content)
+  # File.open("listing.csv", 'wb') {|f| f.write(write_content) }
+
+    csv_final = CSV.generate(options) do |csv|
+
+       csv << column_names
+       all.each do |cellar_items|
+       csv << cellar_items.attributes.values_at(*column_names)
+    #         # @cellar_items = @cellar.items
+    #         # cellar_items.each do |item|
+    #         # wine_detail = item.wine_detail 
+    #         # csv << 
+    #         #    wine_detail.show_year 
+    #         #    wine_detail.wine.name_zh
+    #         #    wine_detail.show_capacity
+    #         #    item.number
+    #         #    item.buy_date.strftime("%Y年%m月%d日") unless item.buy_date.blank?
+    #         #    number_to_currency(item.price,  :unit => "CNY")
+    #         #    item.location
+    #         end
+    #     # end
+       end
+    end
+  end
+
 end
