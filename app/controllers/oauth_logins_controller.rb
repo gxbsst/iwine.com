ce8bba1b246ec492f26f100eebd89c0fdd71c20a
shanhua <shanhua.chen@sidways.com>
2012-07-24 10:13:38 +0800

    第三方登陆

--- app/controllers/oauth_logins_controller.rb
+++ app/controllers/oauth_logins_controller.rb
@@ -1,5 +1,6 @@
 
 class OauthLoginsController < ApplicationController
+
   def sns_login
   	sns_class_name = params[:sns_name].capitalize
     oauth_module = eval( "OauthChina::#{sns_class_name}" )
@@ -11,6 +12,34 @@ class OauthLoginsController < ApplicationController
   end
 
   def update_info
+  	#向第三方请求用户信息
+  	sns_class_name = params[:type].capitalize
+    oauth_module = eval( "OauthChina::#{sns_class_name}" )
+    client = oauth_module.load(Rails.cache.read(build_oauth_token_key(params[:type], params[:oauth_token])))
+    client.authorize(:oauth_verifier => params[:oauth_verifier])
+    results = client.dump
+
+    if results[:access_token] && results[:access_token_secret]
+      flash[:notice] = "done"
+    else
+      flash[:notice] = "fail"
+    end
+    # 创建UserOauth记录
+    user_oauth = Users::Oauth.login_oauth.where("sns_name = ? and sns_user_id = ?", client.name.to_s, client.user_id)
+    if user_oauth.present?
+      
+    else
+
+    end
+    user_oauth = current_user.oauth_token client.name.to_s
+    user_oauth.access_token = results[:access_token]
+    user_oauth.sns_user_id = client.user_id # sns.rb Sina#user_id
+    user_oauth.refresh_token = results[:access_token_secret]
+    user_oauth.save
+
+  end
+
+  def sns_first_login
 
   end
 
