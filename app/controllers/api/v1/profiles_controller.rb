ca769fe9ff63306e3e096458a29fa7c8656a7179
Weston <weston.wei@sidways.com>
2012-08-02 14:17:41 +0800

    Merge profile controller
    Signed-off-by: Weston <weston.wei@sidways.com>

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -10,12 +10,17 @@ module Api
       # :birthday => "yyyy-MM-dd", 
       # :bio => "..big text.."}}
       def update
-        params[:user][:profile_attributes][:birthday] = Time.parse params[:user][:profile_attributes][:birthday]
+
+        birthday = params[:user][:profile_attributes][:birthday]
+        if birthday.present? 
+          params[:user][:profile_attributes][:birthday] = Time.parse birthday
+        end
+
         if current_user.update_attributes(params[:user]) &&
           current_user.profile.update_attributes(params[:user][:profile_attributes])
           success_json(current_user)
         else 
-         invalid_update_json(current_user) 
+          invalid_update_json(current_user) 
         end
       end
 
