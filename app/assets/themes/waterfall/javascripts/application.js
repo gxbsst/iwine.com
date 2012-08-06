b29adf4eb4dd53404a591e6ecd1fb9c3474b8370
Weston <weixuhong@gmail.com>
2012-08-06 15:03:27 +0800

    Make Select Region Work.

--- app/assets/themes/waterfall/javascripts/application.js
+++ app/assets/themes/waterfall/javascripts/application.js
@@ -25,7 +25,7 @@
 //= require waterfall/javascripts/home
 //= require waterfall/javascripts/css_browser_selector
 //= require waterfall/javascripts/autocomplete_plus
-//= require region_word
+//= require waterfall/JavaScripts/region_world
 
 
 $(document).ready(function(){
