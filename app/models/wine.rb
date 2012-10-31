77f10b432a3691e463cde9a57cc319b5bd4db47c
shanhua <shanhua.chen@sidways.com>
2012-10-31 20:42:13 +0800

    去掉wine_detail中的is_nv

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -63,7 +63,7 @@ class Wine < ActiveRecord::Base
   def copy_detail(detail, is_nv, year)
     #查找detail
     if is_nv
-      wine_detail = details.where("is_nv = ?", 1).first
+      wine_detail = details.first
     else
       wine_detail = details.where("year like ?", "#{year}%").first
     end
@@ -72,11 +72,7 @@ class Wine < ActiveRecord::Base
     else
       #创建detail
       wine_detail = Wines::Detail.new(:wine_id => id)
-      if is_nv
-        wine_detail.is_nv = true 
-      else
-        wine_detail.year = Time.mktime(year) 
-      end
+      wine_detail.year = Time.mktime(year) 
       wine_detail.save
       return wine_detail
     end
--- db/migrate/20121031100935_add_is_nv_again_to_wine_details.rb
+++
@@ -1,5 +0,0 @@
-class AddIsNvAgainToWineDetails < ActiveRecord::Migration
-  def change
-    add_column :wine_details, :is_nv, :boolean, :default => false
-  end
-end
