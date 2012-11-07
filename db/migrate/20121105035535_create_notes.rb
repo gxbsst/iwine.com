4f13ffa189309d1b15535a39fa212597d176ef60
Weston <weixuhong@gmail.com>
2012-11-07 12:09:56 +0800

    更新表notes

--- db/migrate/20121105035535_create_notes.rb
+++ db/migrate/20121105035535_create_notes.rb
@@ -2,30 +2,30 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes, :id => false do |t|
       t.string :id, :limit => 36
-      t.belongs_to  :user
-      t.belongs_to  :wine_detail
-      t.belongs_to  :wine_region_tree
-      t.belongs_to  :wine_style
-      t.string      :photo
-      t.integer     :vintage
-      t.string      :name
-      t.string      :other_name
-      t.integer     :rating
-      t.string      :location
+      t.belongs_to  :user         # 用户
+      t.belongs_to  :wine_detail   # 酒
+      t.belongs_to  :wine_region_tree  # 产区
+      t.belongs_to  :wine_style     # 如 红葡萄酒
+      t.string      :photo         # 保存图片
+      t.integer     :vintage       # 年份
+      t.string      :name          # 原始名字
+      t.string      :other_name    # 其他名字
+      t.integer     :rating        # 评星   -1  表示没有评星 0 1 2 3 4
+      t.string      :location      # 地址 地点
       t.text        :grape   # 葡萄类型及百分比 葡萄种类id:10.0;  葡萄种类id:20.0;
-      t.string      :alcohol # 酒精度
+      t.string      :alcohol # 酒精度  格式： 12.5
       t.float       :price   # 价格
       t.text        :comment # 简评
       # 外观
       t.integer     :appearance_clarity # 澄清度 1-1
       t.integer     :appearance_intensity  # 强度 0 - 5
-      t.text        :appearance_color      # 颜色 , 对照jamse表
+      t.text        :appearance_color      # 颜色 , 对照jamse表 //  ATN_Color_Pre_Deep ATN_Color_Red_Purple
       t.text        :appearance_other      # 其他信息
       # 香气
       t.integer     :nose_condition      # 香气状况
       t.integer     :nose_intensity      # 强度
       t.integer     :nose_development    # 发展
-      t.text        :nose_aroma           # 特征
+      t.text        :nose_aroma           # 特征    BlackFruit_BlackCherry;DriedFruit_Prune;BlackFruit_Blackberry
       # 口感
       t.integer     :palate_sweetness      # 甜度
       t.integer     :palate_acidity        # 酸度
@@ -51,6 +51,8 @@ class CreateNotes < ActiveRecord::Migration
       t.string   :la                       # 经度
       t.string   :lo                       # 纬度
       t.integer  :photoSize                # 图片大小
+
+      t.string   :user_agent   # 使用什么上传品酒辞
 
       t.timestamps
     end
